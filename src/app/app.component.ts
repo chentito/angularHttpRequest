e6a9bb74c4ebd03ba682f61a629562c33e95b3be
Carlos Reyes <cvreyes.salazar@gmail.com>
2019-04-23 10:06:12 -0500

    HttpRequest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user.model';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'app-httprequest';
+export class AppComponent implements OnInit {
+  usersS: User[];
+
+  constructor(private dataService: DataService){}
+  
+  ngOnInit(){
+    return this.dataService.getUsers()
+    .subscribe(data => this.usersS = data);
+  }
+
 }
